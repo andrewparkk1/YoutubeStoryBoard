50d5902086442053fa3e56ce8ac7bf5ebf9e8d25
Andrew Park <andysampark@gmail.com>
2021-01-16 23:42:28 -0500

    did it again

---
+++ YoutubeStoryBoard/ConstantsTemplate.swift
@@ -0,0 +1,15 @@
+////
+////  Constant.swift
+////  YoutubeApp
+////
+////  Created by Andrew Park on 1/16/21.
+////
+//
+//import Foundation
+//
+//struct Constants {
+//    static var API_KEY = ""
+//    static var PLAYLIST_ID = ""
+//    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+//
+//}
